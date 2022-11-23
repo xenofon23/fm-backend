7579a68c711233baac22624bd5e480dbcfb3f38d
xenofon pantsos <pantsosxen23@gmail.com>
2022-11-23 11:14:24 +0200

    ADD UI

--- src/fm/backend/FmBackend.java
+++ src/fm/backend/FmBackend.java
@@ -15,7 +15,7 @@ public class FmBackend {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new startPage().setVisible(true);
     }
     
 }
